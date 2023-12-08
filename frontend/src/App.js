fb4c9a65ad58f5e91e228225317c86398745d8eb
Simran <simrangupta8770@gmail.com>
2023-12-08 19:51:07 +0530

    revised

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import {createContext, useContext,useState,useEffect} from 'react';
-import Speciali from './components/Speciali';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Doctors from './components/Doctors';
-export const DataContext = createContext(null);
-
+import Navbar from './Navbar';
 function App() {
-  const [data,setData]=useState();
-  const fetchApi=async()=>{
-    const res= await fetch('https://carejarintern.onrender.com/api/specializations');
-    const d=await res.json();
-    console.log(d);
-    setData(d);
-  }
-  useEffect(()=>{
-    fetchApi();
-    //console.log(data);
-    
-  },[])
   return (
-    <>
-    {data && <>
-    <DataContext.Provider value={data}>
-    <BrowserRouter>
-    <Routes>
-        <Route exact path="/" element={<Speciali />} />
-          <Route path="/doctors" element={<Doctors />} />
-       
-      </Routes>
-      
-      {/* {data && <img src={`https://randomuser.me/api/portraits/men/${Math.floor(Math.random()*100)+1}.jpg`} alt="img"/>} */}
-      </BrowserRouter>
-    </DataContext.Provider>
-    </>
-    }
-    </>
+    <div className="App">
+     <Navbar />
+    </div>
   );
 }
 
